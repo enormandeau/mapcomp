ab463577085fdfd79cd90928a1cda6718949ac8d
Eric Normandeau <eric.normandeau.qc@gmail.com>
2016-01-11 10:06:29 -0500

    Harden bash scripts

--- 01_scripts/01_bwa_w_fa.sh
+++ 01_scripts/01_bwa_w_fa.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # use bwa mem to map map loci in fasta format against reference genome
 
-# global variables (note: point to REFERENCEGENOME)
-INPUT_FASTA="markers.fasta"
-REFERENCEGENOME="02_raw_data/genome/genome.fasta"
-NUMPROCESSORS=16
+# global variables (note: point to REFERENCE_GENOME)
+INPUT_FASTA="02_raw_data/markers.fasta"
+REFERENCE_GENOME="02_raw_data/genome/genome.fasta"
 INPUT_FOLDER="02_raw_data"
 MAPPED_FOLDER="03_mapped"
+NUMPROCESSORS=16
 
 # Notes:
 # Assumes reference is already indexed
@@ -17,34 +17,28 @@ MAPPED_FOLDER="03_mapped"
 echo "Running MapComp"
 
 # Extract informations for figures later
-echo -e "Species\tLG\tTotalPosition" > $INPUT_FOLDER/$INPUT_FASTA.temp
-grep ">" $INPUT_FOLDER/$INPUT_FASTA |
+echo -e "Species\tLG\tTotalPosition" > ${INPUT_FASTA}.temp
+grep ">" ${INPUT_FASTA} |
     perl -pe 's/>//; s/_/\t/g' |
-    awk 'BEGIN {OFS="\t"} {print $1,$2,$4}' >> $INPUT_FOLDER/$INPUT_FASTA.temp
+    awk 'BEGIN {OFS="\t"} {print $1,$2,$4}' >> ${INPUT_FASTA}.temp
 
-./01_scripts/utility_scripts/find_min_max_total_positions.py $INPUT_FOLDER/$INPUT_FASTA.temp $INPUT_FOLDER/$INPUT_FASTA.info
+./01_scripts/utility_scripts/find_min_max_total_positions.py ${INPUT_FASTA}.temp ${INPUT_FASTA}.info
 
-rm $INPUT_FOLDER/$INPUT_FASTA.temp
+rm ${INPUT_FASTA}.temp
 
 # Map reads using bwa mem 
-input_file=$INPUT_FOLDER/$INPUT_FASTA
-
 # Align reads
 echo "  Aligning reads to genome..."
-bwa mem -t $NUMPROCESSORS $REFERENCEGENOME $input_file > $input_file.sam 2> /dev/null
+bwa mem -t ${NUMPROCESSORS} ${REFERENCE_GENOME} ${INPUT_FASTA} > ${INPUT_FASTA}.sam 2> /dev/null
 
 # keep only mapped reads (-F 4)
 echo "  Filering non-mapped reads..."
-samtools view -Sb -F 4 $input_file.sam > $input_file.mapped_only.bam
+samtools view -Sb -F 4 ${INPUT_FASTA}.sam > ${INPUT_FASTA}.mapped_only.bam
 
 # Sort reads
 echo "  Sorting bam file..."
-samtools sort $input_file.mapped_only.bam $input_file.sorted
-
-# Index bam file
-echo "  Indexing bam file..."
-#samtools index $input_file.sorted.bam
+samtools sort ${INPUT_FASTA}.mapped_only.bam ${INPUT_FASTA}.sorted
 
 # clean temporary files
-rm $input_file.mapped_only.bam
-mv ./$INPUT_FOLDER/*.sam ./$INPUT_FOLDER/*.sorted.bam ./$MAPPED_FOLDER/
+rm ${INPUT_FASTA}.mapped_only.bam
+mv ./${INPUT_FOLDER}/*.sam ./${INPUT_FOLDER}/*.sorted.bam ./${MAPPED_FOLDER}/
--- 01_scripts/02_get_wanted_loci_ids.sh
+++ 01_scripts/02_get_wanted_loci_ids.sh
@@ -5,14 +5,14 @@
 # ./01_scripts/get_wanted_loci_ids.sh bamfile
 
 # Global variables
-BAMFILE=$1
-DIRECTORY=$(dirname $1)
+BAMFILE="$1"
+DIRECTORY="$(dirname $1)"
 
 # Filter bam file
 echo "  Filtering non-unique or bad quality alignments..."
-samtools view $BAMFILE | \
+samtools view ${BAMFILE} | \
     awk '$5 >= 10 {print $1}' | \
     sort | \
     uniq -c | \
     sort -nr | \
-    awk '$1 == 1 {print $2}' > $DIRECTORY/wanted_loci.ids
+    awk '$1 == 1 {print $2}' > ${DIRECTORY}/wanted_loci.ids
