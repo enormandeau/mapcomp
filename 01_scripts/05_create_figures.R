ab98e1bb8cbb98fbec29af622eb377d2d295a248
Sébastien Letort <sebastien.letort@irisa.fr>
2016-03-12 22:32:50 +0000

    if previous is ok, this one correct error in R script. Parameters are now really used. and it is the correct param that are used in mapcomp.

--- 01_scripts/05_create_figures.R
+++ 01_scripts/05_create_figures.R
@@ -9,13 +9,13 @@ args = commandArgs( TRUE )
 # Global variables
 # default value set for retro-compatibility reason
 x = args[1]
-pairs_file = ifelse( exists( x ), x, "03_mapped/wanted_loci.info" )
+pairs_file = ifelse( exists( "x" ), x, "03_mapped/wanted_loci.info" )
 x = args[2]
-loci_file  = ifelse( exists( x ), x, "02_data/markers.fasta.info" )
+loci_file  = ifelse( exists( "x" ), x, "02_data/markers.fasta.info" )
 x <- args[3]
-output_lg_correspondance = ifelse( exists( x ), x, "05_results/linkage_group_correspondance.csv" )
+output_lg_correspondance = ifelse( exists( "x" ), x, "05_results/linkage_group_correspondance.csv" )
 x <- args[4]
-figure_folder = ifelse( exists( x ), x, "04_figures" )
+figure_folder = ifelse( exists( "x" ), x, "04_figures" )
 
 minimum_number_of_points = 5 # Minimum number hits between 2 LGs to display in dark
 sp1_col=1
